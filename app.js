452de60dfc410397d3d2a97664be4fe6b42a5410
ilyas127 <57403728+ilyas127@users.noreply.github.com>
2020-07-26 22:40:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+// function timer(){
+//     document.write("runing.." +"<br>")
+
+// }
+// setInterval(timer,1000)
+
+
+// function timeout(){
+//     document.write("oky")
+// }
+
+// setTimeout(timeout,5000)
+
+// var interval;
+// var count = 0;
+// function timer(){
+//     count++
+//     document.write(count+"<br>")
+// }
+
+// interval = setInterval(timer,1000)
+
+// function timeout(){
+
+// }
+// setTimeout(function(){
+//     clearInterval(interval)
+// },5000)
+
+
+
+
+var min = 0 ;
+var sec = 0;
+var msec = 0;
+var minHeading= document.getElementById("min")
+var secHeading= document.getElementById("sec")
+var msecHeading= document.getElementById("msec")
+var interval;
+
+function timer (){
+    msec++
+    msecHeading.innerHTML = msec
+    if(msec >= 100){
+        sec++
+        secHeading.innerHTML =sec
+        msec = 0;
+    }
+        else if(sec >=60){
+            min++
+            minHeading.innerHTML = min
+            sec = 0
+        }
+  
+}
+
+
+function start(){
+interval = setInterval(timer,10)
+}
+
+
+function stop(){
+    clearInterval(interval)
+}
+
+
+function reset(){
+    min = 0
+    sec = 0
+    msec = 0 
+    minHeading.innerHTML = min
+    secHeading.innerHTML = sec
+    msecHeading.innerHTML = msec
+    clearInterval(interval)
+}
